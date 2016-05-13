c03b835247e359de4d4924ee17d782e3a6056cb5
Jay Byun <jaybyun@Macintosh.local>
2016-05-13 11:29:27 +1000

    finalised somewhat

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,22 +1,24 @@
 class SessionController < ApplicationController
   def new
+    @disable_forumhead = true
+    @disable_forumnav = true
   end
 
   def create
- player = Player.find_by :email => params[:email]
-   if player.present? && player.authenticate(params[:password])
-     session[:player_id] = player.id
-     redirect_to "/pages/welcome"
-   else
-     flash[:error] = "Incorrect email address or password"
-     redirect_to login_path
- end
-end
+    player = Player.find_by :email => params[:email]
+    if player.present? && player.authenticate(params[:password])
+      session[:player_id] = player.id
+      redirect_to "/pages/welcome"
+    else
+      flash[:error] = "Incorrect email address or password"
+      redirect_to login_path
+    end
+  end
 
-def destroy
-  session[:player_id] = nil
-  flash[:error] = "You have successfully logged out."
-  redirect_to root_path
- end
+  def destroy
+    session[:player_id] = nil
+    flash[:error] = "You have successfully logged out."
+    redirect_to root_path
+  end
 
 end
